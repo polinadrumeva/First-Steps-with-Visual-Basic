9da89eed9f12ef9d59fba4e6322db419c8db0011
Polinadrumeva <polina9230@gmail.com>
2025-05-12 13:28:58 +0300

    Inputs

---
+++ InputsForm/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtFIrstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblGender = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(411, 304)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(272, 98)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseMnemonic = False
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtFIrstName
+        '
+        Me.txtFIrstName.Location = New System.Drawing.Point(151, 56)
+        Me.txtFIrstName.Name = "txtFIrstName"
+        Me.txtFIrstName.Size = New System.Drawing.Size(354, 22)
+        Me.txtFIrstName.TabIndex = 1
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(153, 120)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(351, 22)
+        Me.txtLastName.TabIndex = 2
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(159, 185)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(229, 22)
+        Me.txtGender.TabIndex = 3
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(61, 59)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(75, 16)
+        Me.lblFirstName.TabIndex = 4
+        Me.lblFirstName.Text = "FIrst Name:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(61, 126)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(75, 16)
+        Me.lblLastName.TabIndex = 5
+        Me.lblLastName.Text = "Last Name:"
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(72, 188)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(55, 16)
+        Me.lblGender.TabIndex = 6
+        Me.lblGender.Text = "Gender:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFIrstName)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents txtFIrstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblGender As Label
+End Class
---
+++ InputsForm/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        ''INPUT IN MESSAGE BOX
+        ' Dim stFirstName As String
+
+        ' stFirstName = InputBox("Please enter your First name:")
+
+        ' MessageBox.Show("Hello, " & stFirstName & "!")
+
+        '' INPUT 
+        Dim strFirstName As String
+        Dim strLastName As String
+        Dim strGender As String
+
+        strFirstName = txtFIrstName.Text
+        strLastName = txtLastName.Text
+        strGender = txtGender.Text
+
+        MessageBox.Show("Hello, " & strFirstName & " " & strLastName & vbNewLine &
+                        "You are " & strGender)
+
+    End Sub
+End Class
