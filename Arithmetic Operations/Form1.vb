5041ca2984b54dcbc7788b9d85432e3d8bb5f118
Polinadrumeva <polina9230@gmail.com>
2025-05-14 20:55:49 +0300

    operations

---
+++ Arithmetic Operations/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim iNumber1 As Integer
+        Dim iNumber2 As Integer
+        Dim iResult As Integer
+
+        iNumber1 = txtNumber1.Text
+        iNumber2 = txtNumber2.Text
+
+        iResult = iNumber1 + iNumber2
+
+        MessageBox.Show(iResult)
+
+    End Sub
+End Class
