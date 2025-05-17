e5e5bbcd840d6296f2f856c7a9d5e7c6eef02faf
Polinadrumeva <polina9230@gmail.com>
2025-05-17 21:12:27 +0300

    update mod

--- Grading%Mod Calculator/Form1.Designer.vb
+++ Grading%Mod Calculator/Form1.Designer.vb
@@ -23,22 +23,26 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.pnl = New System.Windows.Forms.Panel()
+        Me.txtModResult = New System.Windows.Forms.TextBox()
+        Me.lblModNumber = New System.Windows.Forms.Label()
+        Me.lblGradingResult = New System.Windows.Forms.Label()
+        Me.txtModNumber = New System.Windows.Forms.TextBox()
+        Me.txtResultGrading = New System.Windows.Forms.TextBox()
+        Me.lblModResult = New System.Windows.Forms.Label()
         Me.lblGradeNumber = New System.Windows.Forms.Label()
         Me.lblNumber = New System.Windows.Forms.Label()
         Me.txtGradeNumber = New System.Windows.Forms.TextBox()
         Me.txtNumber = New System.Windows.Forms.TextBox()
         Me.btnMod = New System.Windows.Forms.Button()
         Me.btnGrading = New System.Windows.Forms.Button()
-        Me.lblModResult = New System.Windows.Forms.Label()
-        Me.txtResultGrading = New System.Windows.Forms.TextBox()
-        Me.txtModNumber = New System.Windows.Forms.TextBox()
-        Me.lblGradingResult = New System.Windows.Forms.Label()
         Me.pnl.SuspendLayout()
         Me.SuspendLayout()
         '
         'pnl
         '
         Me.pnl.BackColor = System.Drawing.Color.DarkGray
+        Me.pnl.Controls.Add(Me.txtModResult)
+        Me.pnl.Controls.Add(Me.lblModNumber)
         Me.pnl.Controls.Add(Me.lblGradingResult)
         Me.pnl.Controls.Add(Me.txtModNumber)
         Me.pnl.Controls.Add(Me.txtResultGrading)
@@ -51,44 +55,98 @@ Partial Class Form1
         Me.pnl.Controls.Add(Me.btnGrading)
         Me.pnl.Location = New System.Drawing.Point(35, 14)
         Me.pnl.Name = "pnl"
-        Me.pnl.Size = New System.Drawing.Size(666, 434)
+        Me.pnl.Size = New System.Drawing.Size(704, 434)
         Me.pnl.TabIndex = 0
+        '
+        'txtModResult
+        '
+        Me.txtModResult.Location = New System.Drawing.Point(229, 344)
+        Me.txtModResult.Name = "txtModResult"
+        Me.txtModResult.Size = New System.Drawing.Size(220, 22)
+        Me.txtModResult.TabIndex = 11
+        '
+        'lblModNumber
+        '
+        Me.lblModNumber.AutoSize = True
+        Me.lblModNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblModNumber.Location = New System.Drawing.Point(398, 139)
+        Me.lblModNumber.Name = "lblModNumber"
+        Me.lblModNumber.Size = New System.Drawing.Size(95, 20)
+        Me.lblModNumber.TabIndex = 10
+        Me.lblModNumber.Text = "Делител:"
+        '
+        'lblGradingResult
+        '
+        Me.lblGradingResult.AutoSize = True
+        Me.lblGradingResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblGradingResult.Location = New System.Drawing.Point(380, 25)
+        Me.lblGradingResult.Name = "lblGradingResult"
+        Me.lblGradingResult.Size = New System.Drawing.Size(273, 22)
+        Me.lblGradingResult.TabIndex = 9
+        Me.lblGradingResult.Text = "Резултат след степенуване:"
+        '
+        'txtModNumber
+        '
+        Me.txtModNumber.Location = New System.Drawing.Point(512, 139)
+        Me.txtModNumber.Name = "txtModNumber"
+        Me.txtModNumber.Size = New System.Drawing.Size(97, 22)
+        Me.txtModNumber.TabIndex = 8
+        '
+        'txtResultGrading
+        '
+        Me.txtResultGrading.Location = New System.Drawing.Point(366, 61)
+        Me.txtResultGrading.Name = "txtResultGrading"
+        Me.txtResultGrading.Size = New System.Drawing.Size(287, 22)
+        Me.txtResultGrading.TabIndex = 7
+        '
+        'lblModResult
+        '
+        Me.lblModResult.AutoSize = True
+        Me.lblModResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblModResult.Location = New System.Drawing.Point(173, 295)
+        Me.lblModResult.Name = "lblModResult"
+        Me.lblModResult.Size = New System.Drawing.Size(320, 22)
+        Me.lblModResult.TabIndex = 6
+        Me.lblModResult.Text = "Резултат след модулно деление:"
         '
         'lblGradeNumber
         '
         Me.lblGradeNumber.AutoSize = True
-        Me.lblGradeNumber.Location = New System.Drawing.Point(129, 67)
+        Me.lblGradeNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblGradeNumber.Location = New System.Drawing.Point(61, 61)
         Me.lblGradeNumber.Name = "lblGradeNumber"
-        Me.lblGradeNumber.Size = New System.Drawing.Size(58, 16)
+        Me.lblGradeNumber.Size = New System.Drawing.Size(89, 20)
         Me.lblGradeNumber.TabIndex = 5
-        Me.lblGradeNumber.Text = " Степен"
+        Me.lblGradeNumber.Text = " Степен:"
         '
         'lblNumber
         '
         Me.lblNumber.AutoSize = True
-        Me.lblNumber.Location = New System.Drawing.Point(39, 145)
+        Me.lblNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblNumber.Location = New System.Drawing.Point(15, 141)
         Me.lblNumber.Name = "lblNumber"
-        Me.lblNumber.Size = New System.Drawing.Size(47, 16)
+        Me.lblNumber.Size = New System.Drawing.Size(72, 20)
         Me.lblNumber.TabIndex = 4
-        Me.lblNumber.Text = "Число"
+        Me.lblNumber.Text = "Число:"
         '
         'txtGradeNumber
         '
-        Me.txtGradeNumber.Location = New System.Drawing.Point(205, 61)
+        Me.txtGradeNumber.Location = New System.Drawing.Point(177, 59)
         Me.txtGradeNumber.Name = "txtGradeNumber"
-        Me.txtGradeNumber.Size = New System.Drawing.Size(58, 22)
+        Me.txtGradeNumber.Size = New System.Drawing.Size(73, 22)
         Me.txtGradeNumber.TabIndex = 3
         '
         'txtNumber
         '
         Me.txtNumber.Location = New System.Drawing.Point(102, 139)
         Me.txtNumber.Name = "txtNumber"
-        Me.txtNumber.Size = New System.Drawing.Size(117, 22)
+        Me.txtNumber.Size = New System.Drawing.Size(138, 22)
         Me.txtNumber.TabIndex = 2
         '
         'btnMod
         '
         Me.btnMod.BackColor = System.Drawing.Color.LightGreen
+        Me.btnMod.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
         Me.btnMod.Location = New System.Drawing.Point(396, 209)
         Me.btnMod.Name = "btnMod"
         Me.btnMod.Size = New System.Drawing.Size(175, 57)
@@ -99,44 +157,13 @@ Partial Class Form1
         'btnGrading
         '
         Me.btnGrading.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnGrading.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
         Me.btnGrading.Location = New System.Drawing.Point(65, 209)
         Me.btnGrading.Name = "btnGrading"
         Me.btnGrading.Size = New System.Drawing.Size(175, 57)
         Me.btnGrading.TabIndex = 0
         Me.btnGrading.Text = "Степенуване"
         Me.btnGrading.UseVisualStyleBackColor = False
-        '
-        'lblModResult
-        '
-        Me.lblModResult.AutoSize = True
-        Me.lblModResult.Location = New System.Drawing.Point(209, 296)
-        Me.lblModResult.Name = "lblModResult"
-        Me.lblModResult.Size = New System.Drawing.Size(223, 16)
-        Me.lblModResult.TabIndex = 6
-        Me.lblModResult.Text = "Резултат след модулно деление"
-        '
-        'txtResultGrading
-        '
-        Me.txtResultGrading.Location = New System.Drawing.Point(398, 61)
-        Me.txtResultGrading.Name = "txtResultGrading"
-        Me.txtResultGrading.Size = New System.Drawing.Size(212, 22)
-        Me.txtResultGrading.TabIndex = 7
-        '
-        'txtModNumber
-        '
-        Me.txtModNumber.Location = New System.Drawing.Point(512, 139)
-        Me.txtModNumber.Name = "txtModNumber"
-        Me.txtModNumber.Size = New System.Drawing.Size(97, 22)
-        Me.txtModNumber.TabIndex = 8
-        '
-        'lblGradingResult
-        '
-        Me.lblGradingResult.AutoSize = True
-        Me.lblGradingResult.Location = New System.Drawing.Point(416, 25)
-        Me.lblGradingResult.Name = "lblGradingResult"
-        Me.lblGradingResult.Size = New System.Drawing.Size(193, 16)
-        Me.lblGradingResult.TabIndex = 9
-        Me.lblGradingResult.Text = "Резултат след степенуване"
         '
         'Form1
         '
@@ -163,4 +190,6 @@ Partial Class Form1
     Friend WithEvents txtResultGrading As TextBox
     Friend WithEvents lblModResult As Label
     Friend WithEvents lblGradingResult As Label
+    Friend WithEvents lblModNumber As Label
+    Friend WithEvents txtModResult As TextBox
 End Class
--- Grading%Mod Calculator/Form1.vb
+++ Grading%Mod Calculator/Form1.vb
@@ -1,30 +1,67 @@
-﻿Public Class Form1
+﻿Imports System.Numerics
+
+Public Class Form1
     Private Sub btnGrading_Click(sender As Object, e As EventArgs) Handles btnGrading.Click
 
         Dim number As Decimal
-        Dim gradeNumber As Decimal
-        Dim resultGrading As Decimal
+        Dim gradeNumber As Integer
+        Dim resultGrading As BigInteger
+
+
+        txtResultGrading.ReadOnly = True
+
 
         If IsNumeric(txtNumber.Text) = False Or IsNumeric(txtGradeNumber.Text) = False Then
             MessageBox.Show("Грешка! Моля въведете число!")
             txtNumber.Clear()
             txtGradeNumber.Clear()
+            txtResultGrading.Clear()
 
+        ElseIf Not Integer.TryParse(txtGradeNumber.Text, gradeNumber) Then
+            MessageBox.Show("Степента трябва да е цяло число!")
+            txtGradeNumber.Clear()
+            txtResultGrading.Clear()
         Else
             number = txtNumber.Text
             gradeNumber = txtGradeNumber.Text
 
+            resultGrading = number ^ gradeNumber
+
+            txtResultGrading.Text = resultGrading.ToString()
+
         End If
 
-        resultGrading = number ^ gradeNumber
-
-        txtResultGrading.ReadOnly = True
-        txtResultGrading.Text = resultGrading
 
 
     End Sub
 
     Private Sub btnMod_Click(sender As Object, e As EventArgs) Handles btnMod.Click
+        Dim resultGrading As BigInteger
+        Dim modNumber As Integer
+        Dim resultMod As BigInteger
+
+
+        txtModResult.ReadOnly = True
+
+        If String.IsNullOrWhiteSpace(txtResultGrading.Text) Then
+            MessageBox.Show("Моля първо изчислете степенуването!")
+        ElseIf IsNumeric(txtModNumber.Text) = False Then
+            MessageBox.Show("Грешка! Моля въведете число!")
+            txtModNumber.Clear()
+        ElseIf Integer.Parse(txtModNumber.Text) > BigInteger.Parse(txtResultGrading.Text) Then
+            MessageBox.Show("Грешка! Невъзможно деление на " & txtModNumber.Text)
+            txtModNumber.Clear()
+        ElseIf Integer.Parse(txtModNumber.Text) = 0 Then
+            MessageBox.Show("Грешка! Деление на нула не е позволено!")
+            txtModNumber.Clear()
+
+        Else
+            resultGrading = txtResultGrading.Text
+            modNumber = txtModNumber.Text
+
+            resultMod = resultGrading Mod modNumber
+            txtModResult.Text = resultMod.ToString()
+        End If
 
     End Sub
 
