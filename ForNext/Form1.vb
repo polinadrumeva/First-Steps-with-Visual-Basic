ca60504f7687ac11eded647430fb0493f474a1ba
Polinadrumeva <polina9230@gmail.com>
2025-05-21 13:45:04 +0300

    add

---
+++ ForNext/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        For index = 1 To 10
+            Beep()
+            Threading.Thread.Sleep(3000)
+        Next
+    End Sub
+End Class
---
+++ Odd or even/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+
+        Dim number As Integer
+        Dim var As String
+        Dim x As Integer
+
+        number = InputBox("What number do you want to count up to?")
+        var = InputBox("Do you want odd numbers ot even numbers?")
+
+        If var = "even" Then
+            For x = 2 To number Step 2
+                MessageBox.Show(x)
+            Next
+        ElseIf var = "odd" Then
+            For x = 1 To number Step 2
+                MessageBox.Show(x)
+
+            Next
+        Else
+            MessageBox.Show("Please enter valid type!")
+
+
+        End If
+
+
+
+    End Sub
+End Class
