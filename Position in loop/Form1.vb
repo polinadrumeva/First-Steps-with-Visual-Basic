04b5cb964a0157d2fa92bbc9c8d2513b9c80fdf1
Polinadrumeva <polina9230@gmail.com>
2025-05-23 09:51:58 +0300

    add position in loop statement lab

---
+++ Position in loop/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim iCount As Integer
+
+        'Difference between position in loop statement
+
+        'Do While
+        Do While iCount < 5
+            MessageBox.Show("Hello " & iCount)
+            iCount += 1
+        Loop
+
+        iCount = 0
+        Do
+            MessageBox.Show("Hello " & iCount)
+            iCount += 1
+        Loop While iCount = 5
+
+
+        'Do Until
+        iCount = 0
+        Do Until iCount <= 5
+            MessageBox.Show("Hello " & iCount)
+            iCount += 1
+        Loop
+
+        iCount = 0
+        Do
+            MessageBox.Show("Hello " & iCount)
+            iCount += 1
+        Loop Until iCount = 5
+    End Sub
+End Class
---
+++ Position in loop/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Position in loop")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Position in loop")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bb172290-5fdb-42e9-9715-ea50cd0d2873")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
