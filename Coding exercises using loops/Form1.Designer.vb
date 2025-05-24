a2b28d73ecb5f051584938f97852db6816fa6b9e
Polinadrumeva <polina9230@gmail.com>
2025-05-24 09:55:14 +0300

    start Coding exercises program

---
+++ Coding exercises using loops/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class btnEachPart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.lblStart = New System.Windows.Forms.Label()
+        Me.lblExer1 = New System.Windows.Forms.Label()
+        Me.lblInitalData = New System.Windows.Forms.Label()
+        Me.lbInitialData = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(42, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(148, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Exercise 1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(42, 141)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(148, 37)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(42, 206)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(148, 37)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(42, 271)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(148, 37)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(42, 336)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(148, 37)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(42, 401)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(148, 37)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(42, 466)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(148, 37)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(42, 531)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(148, 37)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'lblStart
+        '
+        Me.lblStart.AutoSize = True
+        Me.lblStart.Font = New System.Drawing.Font("Kristen ITC", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStart.Location = New System.Drawing.Point(93, 27)
+        Me.lblStart.Name = "lblStart"
+        Me.lblStart.Size = New System.Drawing.Size(812, 30)
+        Me.lblStart.TabIndex = 8
+        Me.lblStart.Text = "C o d i n g   e x e r c i s e s   u s i n g   a r r a y s   w i t h   l o o p s"
+        '
+        'lblExer1
+        '
+        Me.lblExer1.AutoSize = True
+        Me.lblExer1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblExer1.Location = New System.Drawing.Point(201, 81)
+        Me.lblExer1.Name = "lblExer1"
+        Me.lblExer1.Size = New System.Drawing.Size(493, 22)
+        Me.lblExer1.TabIndex = 9
+        Me.lblExer1.Text = "Output each item in separate message box one after another"
+        '
+        'lblInitalData
+        '
+        Me.lblInitalData.AutoSize = True
+        Me.lblInitalData.Font = New System.Drawing.Font("Kristen ITC", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInitalData.Location = New System.Drawing.Point(921, 81)
+        Me.lblInitalData.Name = "lblInitalData"
+        Me.lblInitalData.Size = New System.Drawing.Size(117, 24)
+        Me.lblInitalData.TabIndex = 10
+        Me.lblInitalData.Text = "Initial Data"
+        Me.lblInitalData.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbInitialData
+        '
+        Me.lbInitialData.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.lbInitialData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbInitialData.FormattingEnabled = True
+        Me.lbInitialData.ItemHeight = 16
+        Me.lbInitialData.Location = New System.Drawing.Point(834, 128)
+        Me.lbInitialData.Name = "lbInitialData"
+        Me.lbInitialData.Size = New System.Drawing.Size(287, 258)
+        Me.lbInitialData.TabIndex = 11
+        '
+        'btnEachPart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ClientSize = New System.Drawing.Size(1128, 618)
+        Me.Controls.Add(Me.lbInitialData)
+        Me.Controls.Add(Me.lblInitalData)
+        Me.Controls.Add(Me.lblExer1)
+        Me.Controls.Add(Me.lblStart)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "btnEachPart"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents lblStart As Label
+    Friend WithEvents lblExer1 As Label
+    Friend WithEvents lblInitalData As Label
+    Friend WithEvents lbInitialData As ListBox
+End Class
---
+++ Coding exercises using loops/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class btnEachPart
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblExer1.Click
+
+    End Sub
+End Class
