bdf9d9be56943cc2b0e91ddf397a317a6f38a1c1
Polinadrumeva <polina9230@gmail.com>
2025-05-24 10:17:33 +0300

    done exercise 1 and 2

--- Coding exercises using loops/Form1.Designer.vb
+++ Coding exercises using loops/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class btnEachPart
+Partial Class ArraysExercises
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,8 +22,8 @@ Partial Class btnEachPart
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnExercise1 = New System.Windows.Forms.Button()
+        Me.btnExercise2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
@@ -31,30 +31,31 @@ Partial Class btnEachPart
         Me.Button7 = New System.Windows.Forms.Button()
         Me.Button8 = New System.Windows.Forms.Button()
         Me.lblStart = New System.Windows.Forms.Label()
-        Me.lblExer1 = New System.Windows.Forms.Label()
+        Me.lblExercise1 = New System.Windows.Forms.Label()
         Me.lblInitalData = New System.Windows.Forms.Label()
         Me.lbInitialData = New System.Windows.Forms.ListBox()
+        Me.lblExercise2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnExercise1
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Button1.Location = New System.Drawing.Point(42, 76)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(148, 37)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Exercise 1"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.btnExercise1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnExercise1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExercise1.Location = New System.Drawing.Point(42, 76)
+        Me.btnExercise1.Name = "btnExercise1"
+        Me.btnExercise1.Size = New System.Drawing.Size(148, 37)
+        Me.btnExercise1.TabIndex = 0
+        Me.btnExercise1.Text = "Exercise 1"
+        Me.btnExercise1.UseVisualStyleBackColor = False
         '
-        'Button2
+        'btnExercise2
         '
-        Me.Button2.Location = New System.Drawing.Point(42, 141)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(148, 37)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnExercise2.Location = New System.Drawing.Point(42, 141)
+        Me.btnExercise2.Name = "btnExercise2"
+        Me.btnExercise2.Size = New System.Drawing.Size(148, 37)
+        Me.btnExercise2.TabIndex = 1
+        Me.btnExercise2.Text = "Exercise 2"
+        Me.btnExercise2.UseVisualStyleBackColor = True
         '
         'Button3
         '
@@ -120,21 +121,21 @@ Partial Class btnEachPart
         Me.lblStart.TabIndex = 8
         Me.lblStart.Text = "C o d i n g   e x e r c i s e s   u s i n g   a r r a y s   w i t h   l o o p s"
         '
-        'lblExer1
+        'lblExercise1
         '
-        Me.lblExer1.AutoSize = True
-        Me.lblExer1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
-        Me.lblExer1.Location = New System.Drawing.Point(201, 81)
-        Me.lblExer1.Name = "lblExer1"
-        Me.lblExer1.Size = New System.Drawing.Size(493, 22)
-        Me.lblExer1.TabIndex = 9
-        Me.lblExer1.Text = "Output each item in separate message box one after another"
+        Me.lblExercise1.AutoSize = True
+        Me.lblExercise1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblExercise1.Location = New System.Drawing.Point(196, 81)
+        Me.lblExercise1.Name = "lblExercise1"
+        Me.lblExercise1.Size = New System.Drawing.Size(493, 22)
+        Me.lblExercise1.TabIndex = 9
+        Me.lblExercise1.Text = "Output each item in separate message box one after another"
         '
         'lblInitalData
         '
         Me.lblInitalData.AutoSize = True
         Me.lblInitalData.Font = New System.Drawing.Font("Kristen ITC", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblInitalData.Location = New System.Drawing.Point(921, 81)
+        Me.lblInitalData.Location = New System.Drawing.Point(883, 89)
         Me.lblInitalData.Name = "lblInitalData"
         Me.lblInitalData.Size = New System.Drawing.Size(117, 24)
         Me.lblInitalData.TabIndex = 10
@@ -147,20 +148,31 @@ Partial Class btnEachPart
         Me.lbInitialData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.lbInitialData.FormattingEnabled = True
         Me.lbInitialData.ItemHeight = 16
-        Me.lbInitialData.Location = New System.Drawing.Point(834, 128)
+        Me.lbInitialData.Location = New System.Drawing.Point(807, 141)
         Me.lbInitialData.Name = "lbInitialData"
-        Me.lbInitialData.Size = New System.Drawing.Size(287, 258)
+        Me.lbInitialData.Size = New System.Drawing.Size(287, 306)
         Me.lbInitialData.TabIndex = 11
         '
-        'btnEachPart
+        'lblExercise2
+        '
+        Me.lblExercise2.AutoSize = True
+        Me.lblExercise2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblExercise2.Location = New System.Drawing.Point(196, 146)
+        Me.lblExercise2.Name = "lblExercise2"
+        Me.lblExercise2.Size = New System.Drawing.Size(539, 22)
+        Me.lblExercise2.TabIndex = 12
+        Me.lblExercise2.Text = "Output all of the items in the same message box on separate lines "
+        '
+        'ArraysExercises
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.HighlightText
         Me.ClientSize = New System.Drawing.Size(1128, 618)
+        Me.Controls.Add(Me.lblExercise2)
         Me.Controls.Add(Me.lbInitialData)
         Me.Controls.Add(Me.lblInitalData)
-        Me.Controls.Add(Me.lblExer1)
+        Me.Controls.Add(Me.lblExercise1)
         Me.Controls.Add(Me.lblStart)
         Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.Button7)
@@ -168,17 +180,17 @@ Partial Class btnEachPart
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "btnEachPart"
+        Me.Controls.Add(Me.btnExercise2)
+        Me.Controls.Add(Me.btnExercise1)
+        Me.Name = "ArraysExercises"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents btnExercise2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
@@ -186,7 +198,8 @@ Partial Class btnEachPart
     Friend WithEvents Button7 As Button
     Friend WithEvents Button8 As Button
     Friend WithEvents lblStart As Label
-    Friend WithEvents lblExer1 As Label
+    Friend WithEvents lblExercise1 As Label
     Friend WithEvents lblInitalData As Label
     Friend WithEvents lbInitialData As ListBox
+    Friend WithEvents lblExercise2 As Label
 End Class
--- Coding exercises using loops/Form1.vb
+++ Coding exercises using loops/Form1.vb
@@ -1,5 +1,36 @@
-﻿Public Class btnEachPart
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblExer1.Click
+﻿Public Class ArraysExercises
 
+    'Class-level array – accessible to all methods
+    Dim arrNumber(4) As Integer
+    Dim i As Integer
+
+    'Constructor
+    Public Sub New()
+        'This calls InitializeComponent to load the form components.
+        InitializeComponent()
+
+        'Array initialization
+        arrNumber(0) = 5
+        arrNumber(1) = 7
+        arrNumber(2) = 45
+        arrNumber(3) = 18
+        arrNumber(4) = 59
+    End Sub
+
+    Private Sub btnExercise1_Click(sender As Object, e As EventArgs) Handles btnExercise1.Click
+        For i = 0 To arrNumber.Length - 1
+            MessageBox.Show("Current number is: " & arrNumber(i))
+        Next
+    End Sub
+
+    Private Sub btnExercise2_Click(sender As Object, e As EventArgs) Handles btnExercise2.Click
+
+        Dim strOutput As String
+
+        For i = 0 To arrNumber.Length - 1
+            strOutput += arrNumber(i) & vbNewLine
+        Next
+
+        MessageBox.Show("All numbers are: " & vbNewLine & strOutput)
     End Sub
 End Class
